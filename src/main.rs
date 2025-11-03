80fe034f0c4583f9e84241928a898108dbf8dce3
Shizuko Nakano <shizukommunist@gmail.com>
2025-11-03 18:16:34 -0300

    actual update on main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
-fn main() {
-    println!("Converter!");
-}
+mod length;
+use std::io;
 
+fn main() {
+    println!("Pick a type of conversion!\n(1) Area   (2) Speed   (3) Length   (4) Time   (5) Temperature");
+    let mut input = String::new();
+
+    let _ = io::stdin().read_line(&mut input);
+    let input: u8 = input.trim().parse().unwrap_or(0);
+    match input {
+        0 => Some(println!("Invalid input!")),
+        1 => todo!(), // todo
+        2 => todo!(), // todo
+        3 => Some(length::conversion()),
+        4 => todo!(), // todo
+        5 => todo!(), // todo
+        _ => Some(println!("Invalid input!")),
+    }
+    .expect("Invalid input")
+}
